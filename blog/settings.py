fc6166e69fed38430e88a62e4e34fe76e3612cb5
fateslime <fateslime1010@gmail.com>
2021-02-21 11:15:59 +0800

    [New] template and databass

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'article',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogdb',
+        'USER': 'dbuser',
+        'PASSWORD':'dbuser',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
