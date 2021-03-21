6377df251da6d9c25de85e39a099c340539904cc
fateslime <fateslime1010@gmail.com>
2021-03-21 11:29:13 +0800

    [NEW]Creat article

--- article/views.py
+++ article/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from article.models import Article,Comment
+from article.forms import ArticleForm
+from django.contrib import messages
 
 def article(request):
     articles={article:Comment.objects.filter(article=article) for article in Article.objects.all()}
     context={'articles':articles}
 
     return render(request, 'article/article.html',context)
+def articleCreate(request):
+    template ='article/articleCreate.html'
+    if request.method =='GET':  
+        return render(request,template,{'articleForm':ArticleForm()})
+
+    articleForm=ArticleForm(request.POST)
+    if not articleForm.is_valid():
+        return render(request, template,{'articleForm':articleForm})
+
+    articleForm.save()
+    messages.success(request,'文章已新增')
+    return redirect('article:article')
+
+
+    
